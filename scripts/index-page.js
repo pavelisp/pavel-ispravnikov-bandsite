292e5825fffd83eaba416d76a7ba30b8fa998089
Pavel Ispravnikov <isp.pavel@gmail.com>
2022-02-17 11:11:38 -0500

    added delete+like comment functionality. Implemented fixes after receiving feedback(applying wrappers,html spacing)

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -4,12 +4,12 @@
 
 const loadComments = () => { axios.get('https://project-1-api.herokuapp.com/comments?api_key=104baf9f-b99e-42b2-a940-473e1f68bbb9')
 .then((res)=>{
-  console.log(res.data)
 res.data.forEach((comment)=>{
   let randomNum = Math.floor(Math.random()*1111)
   displayComment(comment, `url(https://source.unsplash.com/100x100/?person?sig${randomNum})`)});
 })};
 
+ 
 
 // Post comment on form submit
 
@@ -23,9 +23,7 @@ const postComment = (name, comment) => {
       comment: comment
     },
 
-  }).then(()=>{
-    loadComments();
-  })
+  }).then(loadComments)
 }
 
 // Getting references to elements on the page
@@ -44,7 +42,7 @@ const displayComment = (cmnt, portrait) => {
   comment.setAttribute('id', cmnt.id);
 
   const commentWrapper = document.createElement("div");
-  commentWrapper.classList.add("comment__wrapper", "divider");
+  commentWrapper.classList.add("divider", "comment__wrapper");
 
   const commentAvatar = document.createElement("div");
   commentAvatar.classList.add("comment__avatar");
@@ -68,11 +66,52 @@ const displayComment = (cmnt, portrait) => {
   commentText.classList.add("comment__text");
   commentText.innerText = cmnt.comment;
 
+  const commentActions = document.createElement("div");
+  commentActions.classList.add("comment__actions");
+
+  const likeBlock = document.createElement("div");
+  likeBlock.classList.add("like");
+
+  const commentLikeText = document.createElement("span");
+  commentLikeText.innerText = "Like";
+
+  const commentLike = document.createElement("a");
+  commentLike.classList.add("like__button");
+  
+  const commentLikeIcon = document.createElement("i");
+  commentLikeIcon.classList.add("like__icon","fa-solid","fa-heart");
+
+  const commentLikeCount = document.createElement("span");
+  commentLikeCount.classList.add("like__count");
+  commentLikeCount.innerText = cmnt.likes;
+
+  const deleteComment = document.createElement("a");
+  deleteComment.classList.add("comment__delete")
+
+  const deleteCommentText = document.createElement("span");
+  deleteCommentText.innerText = "Delete";
+
+  const deleteCommentIcon = document.createElement("i");
+  deleteCommentIcon.classList.add("comment__delete-icon","fa-solid", "fa-trash-can");
+
   commentHeader.appendChild(commentName);
   commentHeader.appendChild(commentDate);
 
   commentContent.appendChild(commentHeader);
   commentContent.appendChild(commentText);
+
+  commentText.appendChild(commentActions);
+  
+  commentActions.appendChild(deleteComment);
+  commentActions.appendChild(likeBlock);
+
+  likeBlock.appendChild(commentLikeCount);
+  likeBlock.appendChild(commentLike);
+  commentLike.appendChild(commentLikeText);
+  commentLike.appendChild(commentLikeIcon);
+
+  deleteComment.appendChild(deleteCommentIcon);
+  deleteComment.appendChild(deleteCommentText);
 
   commentWrapper.appendChild(commentAvatar);
   commentWrapper.appendChild(commentContent);
@@ -80,6 +119,23 @@ const displayComment = (cmnt, portrait) => {
   comment.appendChild(commentWrapper);
 
   comments.prepend(comment);
+
+     // Like comment button event listener
+
+ commentLike.addEventListener('click', (e)=>{
+  e.preventDefault();
+  e.stopPropagation();
+  axios.put(`https://project-1-api.herokuapp.com/comments/${cmnt.id}/like?api_key=104baf9f-b99e-42b2-a940-473e1f68bbb9`)
+  .then((cmnt)=>{commentLikeCount.innerText = cmnt.data.likes})}
+ )
+
+ deleteComment.addEventListener('click', (e)=>{
+  e.preventDefault();
+  e.stopPropagation();
+  comment.remove();
+  axios.delete(`https://project-1-api.herokuapp.com/comments/${cmnt.id}?api_key=104baf9f-b99e-42b2-a940-473e1f68bbb9`)
+ })
+
 };
 
 // Form event listener
@@ -125,6 +181,7 @@ form.addEventListener("submit", (e) => {
     }
   }
 });
+
 
 // Initial load of the comments
 
